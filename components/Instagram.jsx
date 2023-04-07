8b3afe4ec303b9226c77cfebe1ae2025950a9d34
JoeMcG <joestarson1@gmail.com>
2023-04-07 23:12:20 +0100

    Instagram fature WIP

---
+++ components/Instagram.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import img1 from '../public/images/trees.jpg'
+import img2 from '../public/images/2004_1023toppitoct040025-040029 no6.jpg'
+import img3 from '../public/images/CNV00024.jpg'
+import InstagramImg from './InstagramImg'
+
+
+export const Instagram = () => {
+  return (
+    <div>
+        <p>Follow me on Instagram</p>
+    <p>@Captur</p>
+    <div>
+        <InstagramImg />
+    </div>
+
+</div>
+  )
+}
---
+++ components/InstagramImg.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import img1 from '../public/images/trees.jpg'
+import img2 from '../public/images/2004_1023toppitoct040025-040029 no6.jpg'
+import img3 from '../public/images/CNV00024.jpg'
+const InstagramImg = () => {
+  return (
+<div>
+
+<p>Follow me on Instagram</p>
+    <p>@Captur</p>
+    <div></div>
+
+</div>
+    )
+}
+
+export default InstagramImg
